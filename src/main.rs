f6308185efc94a6156ca7bb4e0c778c7e1a2356b
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-01-25 21:02:46 +0100

    config api

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
 #![no_std]
 #![no_main]
 
+use alloc::ffi::CString;
+// when I put wups above wut the program wont compile wtf
+// Order seems to matter (somehow):
+// wups > wut -> doesnt compile (unless -Wl,--allow-multiple-definition but then it wont run)
+// wut > wups -> compiles normally
 use wut::prelude::*;
 use wut::*;
 
-// use wups::prelude::*;
+use wups::prelude::*;
 use wups::*;
 
 WUPS_PLUGIN_NAME!("Rust Plugin");
-WUPS_PLUGIN_DESCRIPTION!("Example plugin");
-WUPS_PLUGIN_VERSION!("v0.1");
-WUPS_PLUGIN_AUTHOR!("29th-day");
-WUPS_PLUGIN_LICENSE!("EUPL");
+
+static HANDLE: sync::RwLock<Option<thread::JoinHandle>> = sync::RwLock::new(None);
+
+struct MyMenu;
+impl ConfigMenu for MyMenu {
+    fn open(root: config::MenuRoot) -> Result<(), config::MenuError> {
+        // todo!()
+
+        root.add(config::Label::new("Label 1"))?;
+
+        let sub = config::Menu::new("Menu 1")?;
+        sub.add(config::Label::new("Menu 1 Label"))?;
+        sub.add(config::Label::new("Menu 2 Label"))?;
+        root.add(sub);
+
+        root.add(config::Label::new("Label 2"))?;
+
+        root.add(config::Toggle::new(
+            "Toggle",
+            "my_super_toggle",
+            true,
+            "On",
+            "Off",
+        ))?;
+
+        root.add(config::Toggle::new(
+            "Toggle 2",
+            "my_super_toggle_2",
+            true,
+            "On",
+            "Off",
+        ))?;
+
+        root.add(config::Range::new("Range", "my_insane_range", 0, -5, 5))?;
+
+        root.add(config::Select::new(
+            "Select",
+            "my_awesome_select",
+            0,
+            vec!["A".to_string(), "B".to_string(), "C".to_string()],
+        ))?;
+
+        Ok(())
+    }
+}
 
 #[on_initialize(Udp)]
 fn init() {
-    wut::logger::init(wut::logger::Udp);
     println!("init");
 
     // println!("{:?}", storage::delete("abc"));
@@ -62,15 +107,48 @@ fn init() {
     //     ],
     // };
     // let _ = ui::MenuUI::new(ui).unwrap();
+
+    let _ = MyMenu::init("My Menu Rust Plugin");
 }
 
 #[on_application_start(Udp)]
 fn start() {
     println!("start");
+
+    // let mut h = HANDLE.write();
+    // if h.is_none() {
+    //     *h = Some(
+    //         thread::Builder::default()
+    //             .name("My Custom Thread")
+    //             .spawn(my_thread)
+    //             .unwrap(),
+    //     );
+    // }
 }
 
-#[on_deinitialize(Udp)]
+fn my_thread() {
+    let _ = logger::init(logger::Udp);
+
+    while thread::current().running() {
+        println!("thread: {}", time::DateTime::now());
+        thread::sleep(time::Duration::from_secs(1));
+    }
+
+    logger::deinit();
+}
+
+#[on_application_exit]
+fn stop() {
+    // println!("stop");
+
+    // let mut h = HANDLE.write();
+    // if let Some(handle) = h.take() {
+    //     handle.thread().cancel();
+    //     println!("{:?}", handle.join());
+    // }
+}
+
+#[on_deinitialize]
 fn deinit() {
-    println!("deinit");
-    wut::thread::sleep(wut::time::Duration::from_secs(3));
+    // println!("deinit");
 }
