f29b2a421f8fb77fcfbddd2860689950caffdd0a
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-01-31 01:30:15 +0100

    function hooks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![no_std]
 #![no_main]
 
-use alloc::ffi::CString;
+extern crate notifications;
+
+use core::any::Any;
+
 // when I put wups above wut the program wont compile wtf
 // Order seems to matter (somehow):
 // wups > wut -> doesnt compile (unless -Wl,--allow-multiple-definition but then it wont run)
@@ -19,14 +22,12 @@ static HANDLE: sync::RwLock<Option<thread::JoinHandle>> = sync::RwLock::new(None
 struct MyMenu;
 impl ConfigMenu for MyMenu {
     fn open(root: config::MenuRoot) -> Result<(), config::MenuError> {
-        // todo!()
-
         root.add(config::Label::new("Label 1"))?;
 
         let sub = config::Menu::new("Menu 1")?;
         sub.add(config::Label::new("Menu 1 Label"))?;
         sub.add(config::Label::new("Menu 2 Label"))?;
-        root.add(sub);
+        root.add(sub)?;
 
         root.add(config::Label::new("Label 2"))?;
 
@@ -52,61 +53,42 @@ impl ConfigMenu for MyMenu {
             "Select",
             "my_awesome_select",
             0,
-            vec!["A".to_string(), "B".to_string(), "C".to_string()],
+            vec!["A", "B", "C"],
         ))?;
 
         Ok(())
     }
 }
 
+// extern "C" {
+//     #[link_section = ".data"]
+//     static mut real_VPADRead: unsafe extern "C" fn(
+//         channel: wut::bindings::VPADChan::Type,
+//         buf: *mut wut::bindings::VPADStatus,
+//         count: u32,
+//         error: *mut wut::bindings::VPADReadError::Type,
+//     ) -> i32;
+// }
+
+static mut INPUT: gamepad::GamepadState = gamepad::GamepadState::empty();
+
+#[function_hook(library = VPAD, function = VPADRead)]
+fn my_VPADRead(
+    chan: wut::bindings::VPADChan::Type,
+    buffers: *mut wut::bindings::VPADStatus,
+    count: u32,
+    error: *mut wut::bindings::VPADReadError::Type,
+) -> i32 {
+    let status = unsafe { hooked.unwrap()(chan, buffers, count, error) };
+
+    unsafe { INPUT = gamepad::GamepadState::from(*buffers) };
+
+    status
+}
+
 #[on_initialize(Udp)]
 fn init() {
     println!("init");
-
-    // println!("{:?}", storage::delete("abc"));
-    // println!("{:?}", storage::load::<String>("abc"));
-    // println!(
-    //     "{:?}",
-    //     storage::store::<String>("abc", "Hello there, Kenobi".to_string())
-    // );
-    // println!("{:?}", storage::load::<String>("abc"));
-
-    // let ui = Root {
-    //     name: PLUGIN_NAME.to_string(),
-    //     items: vec![
-    //         Label {
-    //             text: "X".to_string(),
-    //         },
-    //         Toggle {
-    //             text: "T".to_string(),
-    //             value: true,
-    //             trueValue: "on".to_string(),
-    //             falseValue: "off".to_string(),
-    //             changed: (),
-    //         },
-    //         Range {
-    //             text: "R1".to_string(),
-    //             value: 0,
-    //             min: -5,
-    //             max: 5,
-    //             changed: (),
-    //         },
-    //         Range {
-    //             text: "R2".to_string(),
-    //             value: 0,
-    //             min: 0,
-    //             max: 10,
-    //             changed: (),
-    //         },
-    //         Select {
-    //             text: "S".to_string(),
-    //             index: 0,
-    //             options: vec![c"A", c"B", c"C"],
-    //             changed: (),
-    //         },
-    //     ],
-    // };
-    // let _ = ui::MenuUI::new(ui).unwrap();
 
     let _ = MyMenu::init("My Menu Rust Plugin");
 }
@@ -115,40 +97,135 @@ fn init() {
 fn start() {
     println!("start");
 
-    // let mut h = HANDLE.write();
-    // if h.is_none() {
-    //     *h = Some(
-    //         thread::Builder::default()
-    //             .name("My Custom Thread")
-    //             .spawn(my_thread)
-    //             .unwrap(),
-    //     );
-    // }
+    //     notifications::info("info: abc").show().unwrap();
+    //     notifications::error("error: abc")
+    //         .callback(|| {
+    //             logger::init(logger::Udp);
+    //             println!("error callback");
+    //             logger::deinit();
+    //         })
+    //         .show()
+    //         .unwrap();
+
+    let mut thread = HANDLE.write();
+    if thread.is_none() {
+        *thread = Some(
+            thread::Builder::default()
+                .name("Rust Thread")
+                .spawn(my_thread)
+                .unwrap(),
+        );
+    }
 }
 
 fn my_thread() {
     let _ = logger::init(logger::Udp);
 
+    let hud = notifications::dynamic("").show().unwrap();
+
+    struct Cursor {
+        pos: usize,
+        max: usize,
+        changed: bool,
+        icon: char,
+    }
+
+    impl Cursor {
+        fn new(max: usize) -> Self {
+            Self {
+                pos: 0,
+                max,
+                changed: true,
+                icon: config::glyphs::CafeGlyphs::ARROW_RIGHT,
+            }
+        }
+
+        fn add(&mut self) {
+            self.pos = (self.pos + 1) % self.max;
+            self.changed = true;
+        }
+
+        fn sub(&mut self) {
+            self.pos = (self.pos + self.max - 1) % self.max;
+            self.changed = true;
+        }
+
+        fn has_changed(&mut self) -> bool {
+            let x = self.changed;
+            self.changed = false;
+            x
+        }
+    }
+
+    let items = vec!["A", "B", "C"];
+    let mut cursor = Cursor::new(items.len());
+
+    fn render_menu(pos: usize, options: &[&str], cursor: char) -> String {
+        format!(
+            "{}",
+            options
+                .iter()
+                .enumerate()
+                .map(|(i, &opt)| if i == pos {
+                    format!("{cursor}{opt}")
+                } else {
+                    format!("\u{3000}{opt}")
+                })
+                .collect::<Vec<_>>()
+                .join("    ") // Ensure consistent spacing
+        )
+    }
+
+    use gamepad::Button as B;
+
     while thread::current().running() {
-        println!("thread: {}", time::DateTime::now());
+        // println!("thread: {}", time::DateTime::now());
+
+        // let input = match gamepad.poll() {
+        //     Ok(s) => s,
+        //     Err(_) => gamepad::GamepadState::empty(),
+        //     // Err(e) => panic!("{:?}", e),
+        // };
+
+        // // println!("{:?}", input.hold);
+
+        // if input.hold.contains(B::L | B::R) && input.trigger.contains(B::Left) {
+        //     cursor.sub();
+        // }
+
+        // if input.hold.contains(B::L | B::R) && input.trigger.contains(B::Right) {
+        //     cursor.add();
+        // }
+
+        // if cursor.has_changed() {
+        //     hud.text(&format!("{}", render_menu(cursor.pos, &items, cursor.icon)));
+        // }
+
+        unsafe {
+            println!("{:?}", INPUT);
+        }
         thread::sleep(time::Duration::from_secs(1));
+        // unsafe {
+        //     wut::bindings::GX2WaitForFlip();
+        //     wut::bindings::GX2WaitForFlip();
+        // }
     }
 
     logger::deinit();
 }
 
-#[on_application_exit]
+#[on_application_exit(Udp)]
 fn stop() {
-    // println!("stop");
+    //     // println!("stop");
 
-    // let mut h = HANDLE.write();
-    // if let Some(handle) = h.take() {
-    //     handle.thread().cancel();
-    //     println!("{:?}", handle.join());
-    // }
+    let mut h = HANDLE.write();
+    if let Some(handle) = h.take() {
+        handle.thread().cancel();
+        println!("{:?}", handle.join());
+    }
 }
 
-#[on_deinitialize]
-fn deinit() {
-    // println!("deinit");
-}
+// #[on_deinitialize]
+// fn deinit() {
+//     // println!("deinit");
+// }
